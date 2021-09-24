bb30ec5e965a6d9eec27843206b65fe32ee62e1c
Abdul Aziz Safudin <me@ziz.ninja>
2021-09-24 12:43:18 +0800

    initial commit

---
+++ functions/src/index.ts
@@ -0,0 +1,23 @@
+import * as functions from "firebase-functions";
+import { Telegraf } from "telegraf";
+
+const bot = new Telegraf(functions.config().telegram.token, {
+	telegram: { webhookReply: true },
+})
+
+// error handling
+// bot.catch((err, ctx) => {
+// 	functions.logger.error('[Bot] Error', err)
+// 	return ctx.reply(`Ooops, encountered an error for ${ctx.updateType}`, err)
+// })
+
+// initialize the commands
+bot.command('/start', (ctx) => ctx.reply('Hello! Send any message and I will copy it.'))
+// copy every message and send to the user
+bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.chat.id, ctx.message))
+
+// handle all telegram updates with HTTPs trigger
+exports.maktabaBot = functions.https.onRequest(async (request, response) => {
+	functions.logger.log('Incoming message', request.body)
+	return await bot.handleUpdate(request.body, response)
+})
